8d929a8844ea2ffe8a796974370e8dd6da845b60
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-02-20 15:35:56 +0100

    Lesson 20. Github auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,16 @@
 import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 export default {
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
 } satisfies NextAuthConfig;
--- src/app/auth/_components/buttons/github-button.tsx
+++ src/app/auth/_components/buttons/github-button.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { signIn } from "next-auth/react";
 import { FaGithub } from "react-icons/fa6";
+import { DEFAULT_CALLBACK_URL } from "../../../../../routes";
 type Props = {
   callbackUrl?: string;
 };
-export default function GithubButton({ callbackUrl }: Props) {
+export default function GithubButton({
+  callbackUrl = DEFAULT_CALLBACK_URL,
+}: Props) {
   return (
-    <div className="flex cursor-pointer items-center justify-center gap-x-3 rounded-xl bg-slate-100 px-16 py-5 dark:bg-slate-800">
+    <div
+      className="flex cursor-pointer items-center justify-center gap-x-3 rounded-xl bg-slate-100 px-16 py-5 dark:bg-slate-800"
+      onClick={() => signIn("github", { callbackUrl })}
+    >
       Continue with github <FaGithub className="h-6 w-6" />
     </div>
   );
