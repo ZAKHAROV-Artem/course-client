7a58be11469d19c9549bcfac5456ae3cdf2270b1
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-02-20 15:21:53 +0100

    Lesson 19. Google auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,11 @@
-import { NextAuthConfig } from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
 export default {
-  providers: [Google({})],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 } satisfies NextAuthConfig;
