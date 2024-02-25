7ed793cb8e55c3a3ddc5bf12274ae4fca3fa3de7
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-02-25 15:29:55 +0100

    Lesson 35. Connecting to socket server & peerjs initialization

--- src/app/[code]/_components/providers/meeting-provider.tsx
+++ src/app/[code]/_components/providers/meeting-provider.tsx
@@ -1,11 +1,40 @@
 "use client";
 
-import { ReactNode } from "react";
+import { usePeer } from "@/hooks/state/use-peer";
+import { useSocket } from "@/hooks/state/use-socket";
+import { ReactNode, useEffect } from "react";
 
 type Props = {
   children: ReactNode;
   joinMeeting: () => void;
 };
 export default function MeetingProvider({ children, joinMeeting }: Props) {
+  const socket = useSocket();
+  const { myPeerId, setMyPeerId, setPeer } = usePeer();
+  useEffect(() => {
+    socket.connect();
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    (async function createPeer() {
+      try {
+        const peer = new (await import("peerjs")).default();
+        setPeer(peer);
+        peer.on("open", (peerId) => {
+          setMyPeerId(peerId);
+        });
+
+        peer.on("error", (err) =>
+          console.log("Failed to setup peer connection", err),
+        );
+      } catch (e) {
+        console.log("Unable to create peer");
+      }
+    })();
+  }, [setMyPeerId, setPeer]);
+
   return <div>{children}</div>;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,20 @@
+import { Socket as ClientSocket } from "socket.io-client";
+
 export type Code = string;
 export type Nullable<T> = T | null;
+export type PeerId = string;
+export type KeyValue<T> = Record<string, T>;
+type PeerUser = {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  peerId: PeerId;
+  muted: boolean;
+  visible: boolean;
+};
+type PeerUserWithSocketId = { socketId: string } & PeerUser;
+
 export type MediaKind = "audio" | "video";
 export type StreamStatus = "loading" | "rejected" | "success";
 export type JoinStatus =
@@ -9,3 +24,28 @@ export type JoinStatus =
   | "accepted"
   | "wait-for-owner"
   | "room-is-full";
+export interface ClientToServerEvents {
+  "user:join-request": ({}: {
+    code: Code;
+    user: PeerUser;
+    ownerId: string;
+  }) => void;
+  "user:accepted": ({}: { code: Code; user: PeerUserWithSocketId }) => void;
+  "user:rejected": ({}: { code: Code; user: PeerUserWithSocketId }) => void;
+  "meeting:join": ({}: { code: Code; user: PeerUser }) => void;
+  "user:toggle-audio": (peerId: PeerId) => void;
+  "user:toggle-video": (peerId: PeerId) => void;
+}
+export interface ServerToClientEvents {
+  "meeting:full": () => void;
+  "user:wait-for-owner": () => void;
+  "user:left": (peerId: PeerId) => void;
+  "user:accepted": ({}: { code: Code; user: PeerUser }) => void;
+  "user:rejected": ({}: { code: Code; user: PeerUser }) => void;
+  "user:join-request": (user: PeerUserWithSocketId) => void;
+  "user:joined": (user: PeerUser) => void;
+  "user:toggled-audio": (peerId: PeerId) => void;
+  "user:toggled-video": (peerId: PeerId) => void;
+}
+
+export type Socket = ClientSocket<ServerToClientEvents, ClientToServerEvents>;
